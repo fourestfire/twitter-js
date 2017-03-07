f88d9d57e3d242a3332b66e0bc16608b726da898
Jonah Livingston <11jbl4@queensu.ca>
2017-03-07 08:30:50 -0800

    up to HTTP

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const nunjucks = require('nunjucks')
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); //when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
 
 app.use(function(request, response, next) {
     // do your logging here
@@ -9,7 +25,7 @@ app.use(function(request, response, next) {
 })
 
 app.get('/', function(request, response){
-    response.send("Welcome!");
+    response.render( 'index', {title: 'Hall of Fame', people: people} )
 });
 
 app.get('/news', function(request, response){
